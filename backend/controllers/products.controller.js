52a07c4508fc0e2327f60ac15a77153d9349e482
prithiv26 <itz.prithiv2006@gmail.com>
2025-11-02 02:13:51 +0530

    Added getFeaturedProducts, getRecommendedProducts, toggleFeaturedProduct,  routes to Product routed

--- backend/controllers/products.controller.js
+++ backend/controllers/products.controller.js
@@ -2,6 +2,15 @@ import { redis } from '../lib/redis.js';
 import Product from '../models/product.model.js'
 import cloudinary from '../lib/cloudinary.js';
 
+async function updateFeaturedProductCache(){
+    try {
+        const updatedProducts = await Product.find({isFeatured: true}).lean()
+        await redis.set('featuredProducts', JSON.stringify(updatedProducts))
+    } catch (error) {
+        console.log("Error in pdateFeaturedProductCache", error.message)
+    }
+}
+
 export const getAllProducts = async (req,res) => {
     try{
         const products = await Product.find({})
@@ -64,3 +73,85 @@ export const createProduct = async (req,res) => {
     }
 }
 
+export const deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        if (!product){
+            return res.status(404).json({
+                message: 'No Product found'
+            })
+        }
+        if (product.image){
+            let publicId = product.image.split('/').pop().split('.')[0]
+            await cloudinary.uploader.destroy(`products/${publicId}`)
+        }
+
+        await Product.findByIdAndDelete(req.params.id)
+        res.status(201).json({
+            message: 'Product deleted successfully'
+        })
+    } catch (error) {
+        console.log("Error in deleteProduct controller", error.message);
+        res.status(500).json({
+            message: 'Internal Server Error'
+        })
+    }
+}
+
+export const getRecommendedProducts = async (req, res) => {
+    try {
+        const recommendedProducts = await Product.aggregate([
+            {
+                $sample: {size: 3}
+            },
+            {
+                $project: {
+                    _id: 1,
+                    name: 1,
+                    description: 1,
+                    price: 1,
+                    image: 1
+                }
+            }
+        ])
+        res.json(recommendedProducts)
+    } catch (error) {
+        console.log("Error in getRecommendedProducts controller", error.message);
+        res.status(500).json({
+            message: 'Internal Server Error'
+        })
+    }
+}
+
+export const getProductsByCategory = async (req, res) => {
+    try {
+        const products = await Product.find({category: req.params.category})
+        if (!products){
+            return res.status(404).json({
+                message: 'No products found in category ${req.params.category}'
+            })
+        }
+        res.json(products)
+    } catch (error) {
+        console.log("Error in getProductsByCategory controller", error.message);
+        res.status(500).json({
+            message: 'Internal Server Error'
+        })
+    }
+}
+
+export const toggleFeaturedProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        product.isFeatured = !product.isFeatured
+        await product.save()
+        await updateFeaturedProductCache()
+        res.json(product)
+
+    } catch (error) {
+        console.log("Error in toggleFeaturedProduct controller", error.message);
+        res.status(500).json({
+            message: 'Internal Server Error'
+        })
+    }
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllProducts , getFeaturedProducts, createProduct} from '../controllers/products.controller.js'
+import { getAllProducts , getFeaturedProducts, createProduct, deleteProduct, getRecommendedProducts, getProductsByCategory, toggleFeaturedProduct} from '../controllers/products.controller.js'
 import { adminAuth, userAuth } from '../middlewares/auth.middleware.js'
 
 const router = express.Router()
@@ -7,8 +7,11 @@ const router = express.Router()
 
 router.get('/', userAuth, adminAuth, getAllProducts)
 router.get('/featured', getFeaturedProducts)
+router.get('/recomended', getRecommendedProducts)
+router.get('/category/:category', getProductsByCategory)
+router.patch('/:id', userAuth, adminAuth, toggleFeaturedProduct)
 router.post('/',  userAuth, adminAuth, createProduct)
-
+router.delete('/:id', userAuth, adminAuth, deleteProduct)
 
 
 
